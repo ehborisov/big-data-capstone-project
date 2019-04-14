303ff8dbcc4428c6c83152f8d5c030ecc3318e40
ehborisov <ehborisov@gmail.com>
2019-04-14 15:46:12 +0300

    Fix top countries query in spark code. Use the efficient query. Add readme.

---
+++ README.md
@@ -0,0 +1,28 @@
+=Hadoop developer capstone project:
+
+This project contains data ingestion pipleine configuration and analysis scrips for a small sample project
+of data processing using Hadoop, Flume, Hive and Spark in the Cloudera quickstart VM.
+
+Usage:
+
+1) Start Flume agent with configuration provided in the module `flume-conf`
+2) Start events-producing python script in the `py_events_producer`
+3) Generate the amount of data you'd like to process. (Flume will output generated events in
+ `/user/cloudera/flume/events/%Y/%m/%d` directory in HDFS using regex interceptors for timestamp column)
+4) Start hive commandline or Hue and execute SQL scripts from `hive_queries`
+5) To execute the last query which requires UDFs, build project with gradle `./gradlew clean build shadowJar`
+and place jars into Hive auxiliary jars directory, and then add them and execute `create function` commands.
+6) For the spark part use the Jar from the `scala_spark_code` module and use the following spark-submit command.
+```bash
+spark-submit --class com.ehborisov.udf.PurchasesAnalysisDF --executor-memory 512m --driver-memory 512m  \
+--conf spark.purchases.events.dir="hdfs://localhost:8020/user/cloudera/flume/events/year=2019/month=04/day={04,05,06,07,08,09,10}/" \
+--conf spark.networks.table="/data/capstone/geodata/GeoLite2-Country-Blocks-IPv4.csv" \
+--conf spark.countries.table="/data/capstone/geodata/GeoLite2-Country-Locations.csv" \
+--conf spark.jdbc.url="jdbc:postgresql://127.0.0.1:5432/capstone" --conf spark.db.user="admin" \
+--conf spark.db.password="12345" \
+/data/capstone/scala_spark_code/build/libs/purchases_analysis-1.0-SNAPSHOT-all.jar
+```
+
+it expects postgres database named `capstone` be available at the default port to export the results. 
+7) To export the results of hive queries into the same database use SQL scripts and Sqoop commands from the `sqoop`
+module.
--- scala_spark_code/src/main/scala/com/ehborisov/udf/PurchasesAnalysisDF.scala
+++ scala_spark_code/src/main/scala/com/ehborisov/udf/PurchasesAnalysisDF.scala
@@ -2,12 +2,15 @@ package com.ehborisov.udf
 
 import java.util.Properties
 
+import com.google.common.net.InetAddresses
 import org.apache.commons.net.util.SubnetUtils
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{col, count, rank, sum, udf}
 import org.apache.spark.sql.types._
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.hive.HiveContext
+
+import scala.io.Source
 
 object PurchasesAnalysisDF {
   def main(args: Array[String]) {
@@ -18,6 +21,9 @@ object PurchasesAnalysisDF {
     val properties = System.getProperties
     val jdbc_properties = new Properties()
     jdbc_properties.setProperty("driver", "org.postgresql.Driver")
+    jdbc_properties.setProperty("user", properties.getProperty("spark.db.user"))
+    jdbc_properties.setProperty("password", properties.getProperty("spark.db.password"))
+
 
     val PURCHASES_SCHEMA = StructType(Seq(
       StructField("product_name", StringType),
@@ -35,17 +41,17 @@ object PurchasesAnalysisDF {
       .schema(PURCHASES_SCHEMA)
       .load(properties.getProperty("spark.purchases.events.dir"))
 
-    val networks = sql.read.format("com.databricks.spark.csv")
-      .option("delimiter", ",")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .load(properties.getProperty("spark.networks.table"))
-
-    val countries = sql.read.format("com.databricks.spark.csv")
-      .option("delimiter", ",")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .load(properties.getProperty("spark.countries.table"))
+//    val networks = sql.read.format("com.databricks.spark.csv")
+//      .option("delimiter", ",")
+//      .option("header", "true")
+//      .option("inferSchema", "true")
+//      .load(properties.getProperty("spark.networks.table"))
+//
+//    val countries = sql.read.format("com.databricks.spark.csv")
+//      .option("delimiter", ",")
+//      .option("header", "true")
+//      .option("inferSchema", "true")
+//      .load(properties.getProperty("spark.countries.table"))
 
     //top 10 most frequently purchased categories
 
@@ -79,22 +85,87 @@ object PurchasesAnalysisDF {
     // Select top 10 countries with the highest money spending using ip addresses table
 
 
-    val is_in_range = (network: String, ip: String) => {
-      try {
-        new SubnetUtils(network).getInfo.isInRange(ip)
-      } catch {
-        case e: Exception => false
+//    val is_in_range = (network: String, ip: String) => {
+//      try {
+//        new SubnetUtils(network).getInfo.isInRange(ip)
+//      } catch {
+//        case e: Exception => false
+//      }
+//    }
+//
+//    val ip_in_range_udf = udf(is_in_range)
+//
+//    purchases
+//      .join(networks.alias("networks"),
+//        ip_in_range_udf(col("networks.network"), col("ip_address")))
+//      .join(countries.alias("countries"),
+//        col("networks.geoname_id") === col("countries.geoname_id"))
+//      .groupBy("country_name")
+//      .agg(sum("price").alias("total_spending"))
+//      .orderBy(col("total_spending").desc)
+//      .limit(10)
+//      .write
+//      .mode("overwrite")
+//      .jdbc(properties.getProperty("spark.jdbc.url"), "top_countries_from_spark", jdbc_properties)
+
+
+    val networks_csv = Source.fromFile(properties.getProperty("spark.networks.table"), "UTF-8")
+      .getLines.map(_.split(","))
+    val countries_csv = Source.fromFile(properties.getProperty("spark.countries.table"), "UTF-8")
+      .getLines.map(_.split(","))
+    networks_csv.next // skip header
+    countries_csv.next // skip header
+
+    val networks = networks_csv.map(item => (item(1), item(0))).toArray
+    val countries_map = countries_csv
+      .map(item => (item(0), item(5).replaceAll("^\"|\"$", "")))
+      .filter(item => item._2.length > 0).toMap
+    val network_by_country = countries_map.keySet.flatMap(
+      k => {
+        val per_country = networks.filter(n => n._1 == k)
+        per_country.map(n => (n._2, countries_map(k)))
+      }
+    )
+
+    val convert_ip = (s: String) => {
+      InetAddresses.coerceToInteger(InetAddresses.forString(s))
+    }
+
+    val extract_network_range = (s: String) => {
+      val utils = new SubnetUtils(s)
+      (utils.getInfo.getLowAddress, utils.getInfo.getHighAddress)
+    }
+
+    val data = network_by_country
+      .map(k => {
+        val network_range = extract_network_range(k._1)
+        (
+          convert_ip(network_range._1),
+          convert_ip(network_range._2),
+          k._2
+        )
+      }).toArray
+
+    val get_ip_country = (s: String) => {
+      val converted = convert_ip(s)
+      val country = data.filter(t => {
+        (converted >= t._1) && (converted <= t._2)
+      })
+      if (country.length > 0) {
+        country(0)._3
+      }
+      else {
+        null
       }
     }
 
-    val ip_in_range_udf = udf(is_in_range)
+    val ip_country = udf(get_ip_country)
 
     purchases
-      .join(networks.alias("networks"),
-        ip_in_range_udf(col("networks.network"), col("ip_address")))
-      .join(countries.alias("countries"),
-        col("networks.geoname_id") === col("countries.geoname_id"))
-      .groupBy("country_name")
+      .select("price", "ip_address")
+      .withColumn("country", ip_country(col("ip_address")))
+      .filter(col("country").isNotNull)
+      .groupBy("country")
       .agg(sum("price").alias("total_spending"))
       .orderBy(col("total_spending").desc)
       .limit(10)
